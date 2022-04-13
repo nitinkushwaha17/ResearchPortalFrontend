6c33a52f463dd961a437773bf537efb0594ad811
Nitin <nk17oct@gmail.com>
2022-04-13 18:00:35 +0530

    milestones carousel

--- src/Data/carousel.js
+++ src/Data/carousel.js
@@ -1,10 +1,10 @@
 /* eslint-disable import/prefer-default-export */
-const pic1 = require('../Assets/chicago.jpg');
-const pic2 = require('../Assets/ny.jpg');
-const pic3 = require('../Assets/la.jpg');
+const pic1 = require('../Assets/Ventilator.jpg');
+const pic2 = require('../Assets/O2_concentrator.png');
+// const pic3 = require('../Assets/la.jpg');
 
 export const SLIDE_INFO = [
   { id: 1, title: 'chicago', image: pic1 },
   { id: 2, title: 'la', image: pic2 },
-  { id: 3, title: 'ny', image: pic3 },
+  // { id: 3, title: 'ny', image: pic3 },
 ];
