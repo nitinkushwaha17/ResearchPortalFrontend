8e70055a059e5415baccc143bcf79bb94a489fbe
Nitin <nk17oct@gmail.com>
2022-04-13 12:19:15 +0530

    milestones

--- src/Data/timeline.js
+++ src/Data/timeline.js
@@ -2,38 +2,41 @@
 export const TIMELINE_CONTENTS = [
   {
     id: '1',
-    title: 'TechX 2020',
-    body: 'IEEE COMPUTER SOCIETY SYP MGAB in collaboration with IEEE SB NIT Durgapur and IntelliSys:Machine Learning Symposium was a huge success with a colossal turnout of over 150 enthusiasts who were too eager to learn and explore.IEEE SB NIT Durgapur extends its heartfelt gratitude to our guest speakers, Dr. Fakhral Yusoff and Mr. Shivam Abhilash for leaving us all dazzled and especially to all the participants for their overwhelming response.IEEE SB Jadavpur University hosted the IEEE TechX Congress 2020: Eastern India Techno-Management Leadership Summit on 28th Feb - 1st March 2020, where it assimilated all students and Young Professionals from various IEEE Sections and students branches.',
-    time: 'July,2021',
+    title:
+      'Secure Architecture for FPGA based IoT Applications with enhanced physical unclonable function',
+    body: 'DST-SERB (Core Research/ EMR scheme)',
+    time: '2018-2021',
   },
   {
     id: '2',
-    title: 'IntelliSys',
-    body: 'IEEE COMPUTER SOCIETY SYP MGAB in collaboration with IEEE SB NIT DuIntelliSys:Machine Learning Symposium was a huge success with a colossal turnout of over 150 enthusiasts who were too eager to learn and explore.IEEE SB NIT Durgapur extends its heartfelt gratitude to our guest speakers, Dr. Fakhral Yusoff and Mr. Shivam Abhilash for leaving us all dazzled and especially to all the participants for their overwhelming response.rgapur and IEEE SB Jadavpur University hosted the IEEE TechX Congress 2020: Eastern India Techno-Management Leadership Summit on 28th Feb - 1st March 2020, where it assimilated all students and Young Professionals from various IEEE Sections and students branches. ',
-    time: 'June,2021',
+    title:
+      'Empower SSE: A Linked Open Data Framework for the Empowerment of Social and Solidarity Economy Agents',
+    body: 'Department of Science and Technology. Ref No. DST/INTPORTUGAL/P- 06/2017 ,(jointly with University of Minho and ISCAP Polytechnic of Porto, Portugal)',
+    time: '2018-2021',
   },
   {
-    id: '3',
-    title: 'SDV 2.0',
-    body: 'IEEE COMPUTER SOCIETY SYP MGAB in collaboration with IEEE SB NIT Durgapur and IEEE SB Jadavpur University hosted the IEEE TechX Congress 2020: Eastern India Techno-Management Leadership Summit on 28th Feb - 1st March 2020, where it assimilated all students and Young IntelliSys:Machine Learning Symposium was a huge success with a colossal turnout of over 150 enthusiasts who were too eager to learn and explore.IEEE SB NIT Durgapur extends its heartfelt gratitude to our guest speakers, Dr. Fakhral Yusoff and Mr. Shivam Abhilash for leaving us all dazzled and especially to all the participants for their overwhelming response.Professionals from various IEEE Sections and students branches.',
-    time: 'May,2021',
+    id: '1',
+    title:
+      'Agent Based Algorithmic Approach for Modeling and Optimization of Indian Railway System',
+    body: 'Department of Science and Technology. Ref No. SB/FTP/ETA-407/2013',
+    time: '2014-2018',
   },
-  {
-    id: '4',
-    title: 'IntelliSys',
-    body: 'IEEE COMPUTER SOCIETY SYP MGAB in collaboration with IEEE SB NIT Durgapur and IEEE SB Jadavpur University hosted the IEEE TechX Congress 2020: Eastern India Techno-Management Leadership Summit on 28th Feb - 1st March 2020, where it assimilated all students and Young Professionals from various IEEE IntelliSys:Machine Learning Symposium was a huge success with a colossal turnout of over 150 enthusiasts who were too eager to learn and explore.IEEE SB NIT Durgapur extends its heartfelt gratitude to our guest speakers, Dr. Fakhral Yusoff and Mr. Shivam Abhilash for leaving us all dazzled and especially to all the participants for their overwhelming response.Sections and students branches. ',
-    time: 'Jan,2021',
-  },
-  {
-    id: '5',
-    title: 'IntelliSys',
-    body: 'IEEE COMPUTER SOCIETY SYP MGAB in collaboration with IEEE SB NIT Durgapur and IEEE SB Jadavpur University hosted the IEEE TechX Congress 2020: Eastern India Techno-Management Leadership Summit on 28th Feb - 1st March 2020, where it assimilated all students and Young IntelliSys:Machine Learning Symposium was a huge success with a colossal turnout of over 150 enthusiasts who were too eager to learn and explore.IEEE SB NIT Durgapur extends its heartfelt gratitude to our guest speakers, Dr. Fakhral Yusoff and Mr. Shivam Abhilash for leaving us all dazzled and especially to all the participants for their overwhelming response. Professionals from various IEEE Sections and students branches. ',
-    time: 'April,2020',
-  },
-  {
-    id: '6',
-    title: 'IntelliSys',
-    body: 'IEEE COMPUTER SOCIETY SYP MGAB in collaboration with IEEE SB NIT Durgapur and IEEE SB Jadavpur University hosted the IEEE TechX Congress 2020: Eastern India Techno-Management Leadership Summit on 28th Feb - 1st March 2020, where it assimilated all students and Young Professionals from various IEEE Sections and students branches.',
-    time: 'Aug,2020',
-  },
+  // {
+  //   id: '4',
+  //   title: 'IntelliSys',
+  //   body: 'IEEE COMPUTER SOCIETY SYP MGAB in collaboration with IEEE SB NIT Durgapur and IEEE SB Jadavpur University hosted the IEEE TechX Congress 2020: Eastern India Techno-Management Leadership Summit on 28th Feb - 1st March 2020, where it assimilated all students and Young Professionals from various IEEE IntelliSys:Machine Learning Symposium was a huge success with a colossal turnout of over 150 enthusiasts who were too eager to learn and explore.IEEE SB NIT Durgapur extends its heartfelt gratitude to our guest speakers, Dr. Fakhral Yusoff and Mr. Shivam Abhilash for leaving us all dazzled and especially to all the participants for their overwhelming response.Sections and students branches. ',
+  //   time: 'Jan,2021',
+  // },
+  // {
+  //   id: '5',
+  //   title: 'IntelliSys',
+  //   body: 'IEEE COMPUTER SOCIETY SYP MGAB in collaboration with IEEE SB NIT Durgapur and IEEE SB Jadavpur University hosted the IEEE TechX Congress 2020: Eastern India Techno-Management Leadership Summit on 28th Feb - 1st March 2020, where it assimilated all students and Young IntelliSys:Machine Learning Symposium was a huge success with a colossal turnout of over 150 enthusiasts who were too eager to learn and explore.IEEE SB NIT Durgapur extends its heartfelt gratitude to our guest speakers, Dr. Fakhral Yusoff and Mr. Shivam Abhilash for leaving us all dazzled and especially to all the participants for their overwhelming response. Professionals from various IEEE Sections and students branches. ',
+  //   time: 'April,2020',
+  // },
+  // {
+  //   id: '6',
+  //   title: 'IntelliSys',
+  //   body: 'IEEE COMPUTER SOCIETY SYP MGAB in collaboration with IEEE SB NIT Durgapur and IEEE SB Jadavpur University hosted the IEEE TechX Congress 2020: Eastern India Techno-Management Leadership Summit on 28th Feb - 1st March 2020, where it assimilated all students and Young Professionals from various IEEE Sections and students branches.',
+  //   time: 'Aug,2020',
+  // },
 ];
